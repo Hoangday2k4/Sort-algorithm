041a7b142501cf1fa61aee91c964ae0b0835020c
Hoangday2k4 <147635139+Hoangday2k4@users.noreply.github.com>
2023-12-04 20:41:17 +0700

    Delete finalexam directory

--- finalexam/display/modeframe/ModeInterface.java
+++
@@ -1,9 +0,0 @@
-package finalexam.display.modeframe;
-
-import finalexam.defaultsorting.Sorting;
-
-public interface ModeInterface{
-    void start() throws InterruptedException;
-    void back();
-    void resetArray();
-}
